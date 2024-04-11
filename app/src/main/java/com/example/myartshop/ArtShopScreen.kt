de2bf70bb4271a98e9f98b7fb58a462c1f32d313
beyandara <beyan97@hotmail.com>
2024-04-11 18:48:47 +0200

    endringer i artshopscreen

--- app/src/main/java/com/example/myartshop/ArtShopScreen.kt
+++ app/src/main/java/com/example/myartshop/ArtShopScreen.kt
@@ -1,8 +1,12 @@
 package com.example.myartshop
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
@@ -27,11 +31,22 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.myartshop.ui.OrderViewModel
+import com.example.myartshop.ui.ui.ImagesScreen
 import com.example.myartshop.ui.ui.StartPageScreen
 
+enum class ArtShopScreen(@StringRes val title:Int) {
+    Start(title = R.string.main_page),
+    PaintingsList(title = R.string.paintings),
+    PaintingViewer(title = R.string.painting_choices),
+    Summary(title = R.string.order_summary)
 
+
+}
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ArtshopAppBar(
@@ -63,6 +78,11 @@ fun ArtShopApp(
     viewModel: OrderViewModel = viewModel(),
     navController: NavHostController = rememberNavController()
 ) {
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    // Get the name of the current screen
+    val currentScreen = ArtShopScreen.valueOf(
+        backStackEntry?.destination?.route ?: ArtShopScreen.Start.name
+    )
 
     Scaffold(
         topBar = {
@@ -72,6 +92,30 @@ fun ArtShopApp(
             )
         }
     ) { innerPadding ->
+        val uiState by viewModel.uiState.collectAsState()
+
+        NavHost(
+            navController = navController,
+            startDestination = ArtShopScreen.Start.name,
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(innerPadding)
+        ) {
+            composable(route = ArtShopScreen.Start.name) {
+                StartPageScreen()
+            }
+
+            composable(route = ArtShopScreen.PaintingViewer.name) {
+                ImagesScreen(TODO()) {
+
+                }
+            }
+
+            composable(route = ArtShopScreen.PaintingViewer.name) {
+                SelectedPhotoScreen()
+            }
+        }
         //val uiState by viewModel.uiState.collectAsState()
 
     }
